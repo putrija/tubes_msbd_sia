110890a7dc5cebc3c482952cbd0be5a2ff0e7439
putrija <91309853+putrija@users.noreply.github.com>
2022-12-01 15:01:23 +0700

    Mantap Bro

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(HariSeeder::class);
+        $this->call(KehadiranSeeder::class);
+        $this->call(PaketSeeder::class);
+        $this->call(RuangSeeder::class);
+        $this->call(UsersSeeder::class);
+    }
+}
