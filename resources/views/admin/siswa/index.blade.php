2f794c187d35f49ec48ad18a1073152e51203a9f
ganteng <erastusganteng1@gmail.com>
2022-12-13 21:06:50 +0700

    Erastus- Perbaikan halaman siswa

--- resources/views/admin/siswa/index.blade.php
+++ resources/views/admin/siswa/index.blade.php
@@ -74,27 +74,42 @@
 		</div>
         <!-- /.card-header -->
         <div class="card-body">
-          <table id="example1" class="table table-bordered table-striped table-hover">
-            <thead>
-                <tr>
-                    <th>No.</th>
-                    <th>Kelas</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($kelas as $data)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $data->nama_kelas }}</td>
-                        <td>
-                            <a href="{{ route('siswa.kelas', Crypt::encrypt($data->id)) }}" class="btn btn-info btn-sm"><i class="nav-icon fas fa-search-plus"></i> &nbsp; Ditails</a>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-          </table>
-        </div>
+            <table id="example1" class="table table-bordered table-striped table-hover">
+              <thead>
+                  <tr>
+                      <th>No.</th>
+                      <th>Nama Siswa</th>
+                      <th>No Induk</th>
+                      <th>Foto</th>
+                      <th>Aksi</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  @foreach ($siswa as $data)
+                      <tr>
+                          <td>{{ $loop->iteration }}</td>
+                          <td>{{ $data->nama_siswa }}</td>
+                          <td>{{ $data->no_induk }}</td>
+                          <td>
+                              <a href="{{ asset($data->foto) }}" data-toggle="lightbox" data-title="Foto {{ $data->nama_siswa }}" data-gallery="gallery" data-footer='<a href="{{ route('siswa.ubah-foto', Crypt::encrypt($data->id)) }}" id="linkFotoGuru" class="btn btn-link btn-block btn-light"><i class="nav-icon fas fa-file-upload"></i> &nbsp; Ubah Foto</a>'>
+                                  <img src="{{ asset($data->foto) }}" width="130px" class="img-fluid mb-2">
+                              </a>
+                              {{-- https://siakad.didev.id/siswa/ubah-foto/{{$data->id}} --}}
+                          </td>
+                          <td>
+                              <form action="{{ route('siswa.destroy', $data->id) }}" method="post">
+                                  @csrf
+                                  @method('delete')
+                                  <a href="{{ route('siswa.show', Crypt::encrypt($data->id)) }}" class="btn btn-info btn-sm mt-2"><i class="nav-icon fas fa-id-card"></i> &nbsp; Detail</a>
+                                  <a href="{{ route('siswa.edit', Crypt::encrypt($data->id)) }}" class="btn btn-success btn-sm mt-2"><i class="nav-icon fas fa-edit"></i> &nbsp; Edit</a>
+                                  <button class="btn btn-danger btn-sm mt-2"><i class="nav-icon fas fa-trash-alt"></i> &nbsp; Hapus</button>
+                              </form>
+                          </td>
+                      </tr>
+                  @endforeach
+              </tbody>
+            </table>
+          </div>
         <!-- /.card-body -->
     </div>
     <!-- /.card -->
