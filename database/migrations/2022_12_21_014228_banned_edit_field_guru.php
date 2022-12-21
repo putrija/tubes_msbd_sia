0e8a45da85ac41ca40769038b23eec615447ea79
ganteng <erastusganteng1@gmail.com>
2022-12-21 09:10:10 +0700

    Erastus - Nambah trigger guru dan siswa

---
+++ database/migrations/2022_12_21_014228_banned_edit_field_guru.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        DB::unprepared("CREATE TRIGGER `banned_edit_fileguru` BEFORE UPDATE ON `guru` 
+        FOR EACH ROW 
+        IF NEW.id <> OLD.id || NEW.id_card <> OLD.id_card || NEW.nip <> OLD.nip THEN 
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT ='Field ini tidak dapat diubah!'; 
+        END IF");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
---
+++ database/migrations/2022_12_21_015719_banned_edit_field_siswa_detail.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared("CREATE TRIGGER `banned_edit_field_siswa` BEFORE UPDATE ON `siswa` 
+        FOR EACH ROW 
+        IF NEW.id <> OLD.id THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT ='Field ini tidak dapat diubah!';
+        END IF");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
