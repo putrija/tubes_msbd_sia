36442705785a96d895d24d1abae8b491060ff74b
PutriAnisa123 <pa374167@gmail.com>
2022-12-26 10:30:39 +0700

    detail guru Co-authored-by: gideonmnrg <gideonmnrg@users.noreply.github.com>
    Signed-off-by: PutriAnisa123 <pa374167@gmail.com>

--- resources/views/admin/guru/details.blade.php
+++ resources/views/admin/guru/details.blade.php
@@ -16,9 +16,321 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
   <body>
-    <div class="text-center">
-    <img src="{{ asset($guru->foto) }}" class="card-img img-details" alt="..." width="10px">
-    </div> 
+    <div class="container">
+      <div class="text-center">
+        <img src="{{ asset($guru->foto) }}" width="150px">
+      </div> 
+    </div>
+    <div class="container mt-5">
+    <form>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Id Card</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->id_card_guru }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Nama Guru</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->nama_guru }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Kode Guru</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->kode_guru }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">NIP</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->nip }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">NUPTK</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->nuptk }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Jenis Kelamin</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->jk }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Email</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->email }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Agama</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->agama }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">No Telepon</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->telp }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">No HP</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->hp }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Tempat Lahir</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->tmp_lahir }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Tanggal Lahir</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->tgl_lahir }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">NIK</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->nik }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">No KK</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->no_kk }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Alamat</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->alamat }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">RT</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->rt }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">RW</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->rw }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Nama Dusun</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->nama_dusun }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Desa/Kelurahan</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->desa_kelurahan }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Kecamatan</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->kecamatan }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Kode Pos</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->kode_pos }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Status Kepegawaian</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->ket_status_kepeg }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Jenis PTK</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->ket_jenis_ptk }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Tugas Tambahan</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->ket_tugas_tambahan }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Tanggal CPNS</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->tanggal_cpns }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">SK CPNS</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->sk_cpns }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">SK Pengangkatan</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->sk_pengangkatan }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">TMT Penagangkatan</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->tmt_pengangkatan }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Lembaga Pendidikan</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->lembaga_pendidikan }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Pangkat Golongan</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->pangkat_golongan }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Nama Ibu Kandung</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->nama_ibu_kandung }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Sumber Gaji</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->sumber_gaji }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Status Perkawinan</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->status_perkawinan }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Nama Suami/Istri</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->nama_suami_istri }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">NIP Suami/Istri</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->nip_suami_istri }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Pekerjaan Sumai/Istri</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->pekerjaan_suami_istri }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">TMT PNS</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->tnt_pns }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Lisensi Kepala Sekolah</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->sudah_lisensi_kepsek }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Diklat Kepegawaian</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->pernah_diklat_kepegawaian }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Keahlian Braille</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->keahlian_braille }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Keahlian Bahasa Isyarat</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->keahlian_bahasa_isyarat }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">NPWP</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->npwp }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Nama Wajib Pajak</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->nama_wajib_pajak }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Kewarganegaraan</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->kewarganegaraan }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Bank</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->bank }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Nomor Rekening Bank</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->nomor_rekening_bank }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Nama Rekening</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->rekening_atas_nama }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Karpeg</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->karpeg }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Karis/Karsu</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->karis_karsu }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Lintang</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->lintang }}" disabled>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Bujur</label>
+        <div class="col-sm-10">
+          <input class="form-control" placeholder="{{ $guru->bujur }}" disabled>
+        </div>
+      </div>
+    </form>
+    </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
