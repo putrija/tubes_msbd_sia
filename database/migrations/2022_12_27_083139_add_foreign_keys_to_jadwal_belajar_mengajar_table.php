70f2bec1731b7ee569618ce9364f6d3bae86acc4
putrija <91309853+putrija@users.noreply.github.com>
2022-12-27 23:42:29 +0700

    Delete Mapel sudah berfungsi -Putrija
    yey udah bisa delete, tapi trash mapel nya belum tersambung :(

--- database/migrations/2022_12_27_083139_add_foreign_keys_to_jadwal_belajar_mengajar_table.php
+++ database/migrations/2022_12_27_083139_add_foreign_keys_to_jadwal_belajar_mengajar_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::table('jadwal_belajar_mengajar', function (Blueprint $table) {
             $table->foreign(['ruang_id'], 'jadwal_belajar_mengajar_ibfk_4')->references(['id'])->on('ruang');
             $table->foreign(['kelas_id'], 'jadwal_belajar_mengajar_ibfk_6')->references(['id'])->on('kelas');
-            $table->foreign(['guru_mengajar_id'], 'jadwal_belajar_mengajar_ibfk_5')->references(['id'])->on('guru_mengajar');
+            $table->foreign(['guru_mengajar_id'], 'jadwal_belajar_mengajar_ibfk_5')->references(['id'])->on('guru_mengajar')->onDelete('cascade');
         });
     }
 
--- database/seeders/MapelSeeder.php
+++ database/seeders/MapelSeeder.php
@@ -19,42 +19,49 @@ class MapelSeeder extends Seeder
         DB::table('mapel')->insert([
             'id' => 1,
             'nama_mapel' => 'Fisika',
-            'kurikulum_id' => '2',
+            'kurikulum_id' => '1',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
         ]);
         DB::table('mapel')->insert([
             'id' => 2,
             'nama_mapel' => 'Kimia',
-            'kurikulum_id' => '2',
+            'kurikulum_id' => '1',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
         ]);
         DB::table('mapel')->insert([
             'id' => 3,
             'nama_mapel' => 'Biologi',
-            'kurikulum_id' => '2',
+            'kurikulum_id' => '1',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
         ]);
         DB::table('mapel')->insert([
             'id' => 5,
             'nama_mapel' => 'Matematika Wajib',
-            'kurikulum_id' => '2',
+            'kurikulum_id' => '1',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
         ]);
         DB::table('mapel')->insert([
             'id' => 6,
             'nama_mapel' => 'Matematika Peminatan',
-            'kurikulum_id' => '2',
+            'kurikulum_id' => '1',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
         ]);
         DB::table('mapel')->insert([
             'id' => 7,
             'nama_mapel' => 'Sosiologi',
-            'kurikulum_id' => '2',
+            'kurikulum_id' => '1',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('mapel')->insert([
+            'id' => 8,
+            'nama_mapel' => 'Ppkn',
+            'kurikulum_id' => '1',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
         ]);
