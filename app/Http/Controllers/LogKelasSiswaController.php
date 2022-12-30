e81bbdaae3697df8fa50b6c05c4e4e88f19dfd92
PutriAnisa123 <pa374167@gmail.com>
2022-12-31 00:46:49 +0700

    Update LogKelasSiswaController.php
    fix log kelas siswa

--- app/Http/Controllers/LogKelasSiswaController.php
+++ app/Http/Controllers/LogKelasSiswaController.php
@@ -9,7 +9,7 @@ class LogKelasSiswaController extends Controller
 {
     public function index(){
 
-        // $data = log_kelas_siswa::all();
-        return view ('admin.log_kelas_siswa.index');
+        $data = log_kelas_siswa::all();
+        return view ('admin.log_kelas_siswa.index', ['data'=>$data]);
     }
 }
